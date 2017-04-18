cbaaace3539431aad8fa2c192474a2504462669b
Rimobul <lubo.sokolovsky@gmail.com>
2017-04-18 09:52:35 +0200

    push notifications changes

--- PushNotifications/PushNotifications/www/scripts/index.js
+++ PushNotifications/PushNotifications/www/scripts/index.js
@@ -1,27 +1,46 @@
-﻿// For an introduction to the Blank template, see the following documentation:
-// http://go.microsoft.com/fwlink/?LinkID=397704
-// To debug code on page load in Ripple or on Android devices/emulators: launch your app, set breakpoints, 
-// and then run "window.location.reload()" in the JavaScript Console.
+﻿/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 (function () {
     "use strict";
 
-    document.addEventListener( 'deviceready', onDeviceReady.bind( this ), false );
+    var client, // Connection to the Azure Mobile App backend
+        store,  // Sqlite store to use for offline data sync
+        syncContext, // Offline data sync context
+        tableName = 'todoitem',
+        todoItemTable; // Reference to a table endpoint on backend
 
+    var pushRegistration = null;
+
+    // Set useOfflineSync to true to use tables from local store.
+    // Set useOfflineSync to false to use tables on the server.
+    var useOfflineSync = false;
+
+    // Add an event listener to call our initialization routine when the host is ready
+    document.addEventListener('deviceready', onDeviceReady, false);
+
+    /**
+     * Event Handler, called when the host is ready
+     *
+     * @event
+     */
     function onDeviceReady() {
-        // Handle the Cordova pause and resume events
-        document.addEventListener( 'pause', onPause.bind( this ), false );
-        document.addEventListener( 'resume', onResume.bind( this ), false );
-        
-        // TODO: Cordova has been loaded. Perform any initialization that requires Cordova here.
-        var parentElement = document.getElementById('deviceready');
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-        createClient();
-    };
-
-    function createClient() {
+        // Create a connection reference to our Azure Mobile Apps backend 
         client = new WindowsAzure.MobileServiceClient('https://diplomamobileapp.azurewebsites.net');
 
         if (useOfflineSync) {
@@ -29,75 +48,281 @@
         } else {
             setup();
         }
+    }
 
-        client.login('google').then(function () {
-            // Create a table reference
-            todoItemTable = client.getTable('todoitem');
-
-            // Refresh the todoItems
-            refreshDisplay();
-
-            // Wire up the UI Event Handler for the Add Item
-            $('#add-item').submit(addItemHandler);
-            $('#refresh').on('click', refreshDisplay);
-
-            // Added to register for push notifications.
-            registerForPushNotifications();
-
-        }, handleError);
-    };
-
+    // Register for Push Notifications. Requires that phonegap-plugin-push be installed.
     function registerForPushNotifications() {
-        // Register for Push Notifications. Requires that phonegap-plugin-push be installed.
-        var pushRegistration = null;
-        function registerForPushNotifications() {
-            pushRegistration = PushNotification.init({
-                android: { senderID: '554708347996' },
-                ios: { alert: 'true', badge: 'true', sound: 'true' },
-                wns: {}
-            });
+        pushRegistration = PushNotification.init({
+            android: { senderID: '554708347996' },
+            ios: { alert: 'true', badge: 'true', sound: 'true' },
+            wns: {}
+        });
 
-            // Handle the registration event.
-            pushRegistration.on('registration', function (data) {
-                // Get the native platform of the device.
-                var platform = device.platform;
-                // Get the handle returned during registration.
-                var handle = data.registrationId;
-                // Set the device-specific message template.
-                if (platform === 'android' || platform === 'Android') {
-                    // Register for GCM notifications.
-                    client.push.register('gcm', handle, {
-                        mytemplate: { body: { data: { message: "{$(messageParam)}" } } }
-                    });
-                } else if (device.platform === 'iOS') {
-                    // Register for notifications.
-                    client.push.register('apns', handle, {
-                        mytemplate: { body: { aps: { alert: "{$(messageParam)}" } } }
-                    });
-                } else if (device.platform === 'windows') {
-                    // Register for WNS notifications.
-                    client.push.register('wns', handle, {
-                        myTemplate: {
-                            body: '<toast><visual><binding template="ToastText01"><text id="1">$(messageParam)</text></binding></visual></toast>',
-                            headers: { 'X-WNS-Type': 'wns/toast' }
-                        }
-                    });
+        // Handle the registration event.
+        pushRegistration.on('registration', function (data) {
+            // Get the native platform of the device.
+            var platform = device.platform;
+            // Get the handle returned during registration.
+            var handle = data.registrationId;
+            // Set the device-specific message template.
+            if (platform == 'android' || platform == 'Android') {
+                // Register for GCM notifications.
+                client.push.register('gcm', handle, {
+                    mytemplate: { body: { data: { message: "{$(messageParam)}" } } }
+                });
+            } else if (device.platform === 'iOS') {
+                // Register for notifications.
+                client.push.register('apns', handle, {
+                    mytemplate: { body: { aps: { alert: "{$(messageParam)}" } } }
+                });
+            } else if (device.platform === 'windows') {
+                // Register for WNS notifications.
+                client.push.register('wns', handle, {
+                    myTemplate: {
+                        body: '<toast><visual><binding template="ToastText01"><text id="1">$(messageParam)</text></binding></visual></toast>',
+                        headers: { 'X-WNS-Type': 'wns/toast' }
+                    }
+                });
+            }
+        });
+
+        pushRegistration.on('notification', function (data, d2) {
+            alert('Push Received: ' + data.message);
+        });
+
+        pushRegistration.on('error', handleError);
+    }
+
+    /**
+     * Set up and initialize the local store.
+     */
+    function initializeStore() {
+        // Create the sqlite store
+        store = new WindowsAzure.MobileServiceSqliteStore();
+
+        // Define the table schema
+        return store.defineTable({
+            name: tableName,
+            columnDefinitions: {
+                id: 'string',
+                deleted: 'boolean',
+                text: 'string',
+                complete: 'boolean',
+                version: 'string'
+            }
+        }).then(function () {
+            // Initialize the sync context
+            syncContext = client.getSyncContext();
+
+            // Define an overly simplified push handler that discards
+            // local changes whenever there is an error or conflict.
+            // Note that a real world push handler will have to take action according
+            // to the nature of conflict.
+            syncContext.pushHandler = {
+                onConflict: function (pushError) {
+                    return pushError.cancelAndDiscard();
+                },
+                onError: function (pushError) {
+                    return pushError.cancelAndDiscard();
                 }
-            });
+            };
 
-            pushRegistration.on('notification', function (data, d2) {
-                alert('Push Received: ' + data.message);
-            });
+            return syncContext.initialize(store);
+        });
+    }
 
-            pushRegistration.on('error', handleError);
+    /**
+     * Set up the tables, event handlers and load data from the server 
+     */
+    function setup() {
+        client.login('google')
+             .then(function () {
+                 // Create a table reference
+                 if (useOfflineSync) {
+                     todoItemTable = client.getSyncTable(tableName);
+                 } else {
+                     todoItemTable = client.getTable(tableName);
+                 }
+
+                 // Refresh the todoItems
+                 refreshDisplay();
+
+                 // Wire up the UI Event Handler for the Add Item
+                 $('#add-item').submit(addItemHandler);
+                 $('#refresh').on('click', refreshDisplay);
+
+                 // Added to register for push notifications.
+                 registerForPushNotifications();
+
+             }, handleError);
+    }
+
+    /**
+     * Refresh the display with items from the table.
+     * If offline sync is enabled, the local table will be synchronized
+     * with the server table before displaying the todo items.
+     */
+    function refreshDisplay() {
+        updateSummaryMessage('Loading Data from Azure');
+
+        if (useOfflineSync) {
+            syncLocalTable().then(displayItems);
+        } else {
+            displayItems();
         }
-    };
+    }
 
-    function onPause() {
-        // TODO: This application has been suspended. Save application state here.
-    };
+    /**
+     * Synchronize local table with the table on the server.
+     * We do this by pushing local changes to the server and then
+     * pulling the latest changes from the server.
+     */
+    function syncLocalTable() {
+        return syncContext
+                    .push()
+                    .then(function () {
+                        return syncContext.pull(new WindowsAzure.Query(tableName));
+                    });
+    }
 
-    function onResume() {
-        // TODO: This application has been reactivated. Restore application state here.
-    };
-} )();
+    /**
+     * Displays the todo items
+     */
+    function displayItems() {
+        // Execute a query for uncompleted items and process
+        todoItemTable
+            .where({ complete: false })     // Set up the query
+            .read()                         // Read the results
+            .then(createTodoItemList, handleError);
+    }
+
+    /**
+     * Updates the Summary Message
+     * @param {string} msg the message to use
+     * @returns {void}
+     */
+    function updateSummaryMessage(msg) {
+        $('#summary').html(msg);
+    }
+
+    /**
+     * Create the DOM for a single todo item
+     * @param {Object} item the Todo Item
+     * @param {string} item.id the ID of the item
+     * @param {bool} item.complete true if the item is completed
+     * @param {string} item.text the text value
+     * @returns {jQuery} jQuery DOM object
+     */
+    function createTodoItem(item) {
+        return $('<li>')
+            .attr('data-todoitem-id', item.id)
+            .append($('<button class="item-delete">Delete</button>'))
+            .append($('<input type="checkbox" class="item-complete">').prop('checked', item.complete))
+            .append($('<div>').append($('<input class="item-text">').val(item.text)));
+    }
+
+    /**
+     * Create a list of Todo Items
+     * @param {TodoItem[]} items an array of todoitem objects
+     * @returns {void}
+     */
+    function createTodoItemList(items) {
+        // Cycle through each item received from Azure and add items to the item list
+        var listItems = $.map(items, createTodoItem);
+        $('#todo-items').empty().append(listItems).toggle(listItems.length > 0);
+        $('#summary').html('<strong>' + items.length + '</strong> item(s)');
+
+        // Wire up the event handlers for each item in the list
+        $('.item-delete').on('click', deleteItemHandler);
+        $('.item-text').on('change', updateItemTextHandler);
+        $('.item-complete').on('change', updateItemCompleteHandler);
+    }
+
+    /**
+     * Handle error conditions
+     * @param {Error} error the error that needs handling
+     * @returns {void}
+     */
+    function handleError(error) {
+        var text = error + (error.request ? ' - ' + error.request.status : '');
+        console.error(text);
+        $('#errorlog').append($('<li>').text(text));
+    }
+
+    /**
+     * Given a sub-element of an LI, find the TodoItem ID associated with the list member
+     *
+     * @param {DOMElement} el the form element
+     * @returns {string} the ID of the TodoItem
+     */
+    function getTodoItemId(el) {
+        return $(el).closest('li').attr('data-todoitem-id');
+    }
+
+    /**
+     * Event handler for when the user enters some text and clicks on Add
+     * @param {Event} event the event that caused the request
+     * @returns {void}
+     */
+    function addItemHandler(event) {
+        var textbox = $('#new-item-text'),
+            itemText = textbox.val();
+
+        updateSummaryMessage('Adding New Item');
+        if (itemText !== '') {
+            todoItemTable.insert({
+                text: itemText,
+                complete: false
+            }).then(displayItems, handleError);
+        }
+
+        textbox.val('').focus();
+        event.preventDefault();
+    }
+
+    /**
+     * Event handler for when the user clicks on Delete next to a todo item
+     * @param {Event} event the event that caused the request
+     * @returns {void}
+     */
+    function deleteItemHandler(event) {
+        var itemId = getTodoItemId(event.currentTarget);
+
+        updateSummaryMessage('Deleting Item in Azure');
+        todoItemTable
+            .del({ id: itemId })   // Async send the deletion to backend
+            .then(displayItems, handleError); // Update the UI
+        event.preventDefault();
+    }
+
+    /**
+     * Event handler for when the user updates the text of a todo item
+     * @param {Event} event the event that caused the request
+     * @returns {void}
+     */
+    function updateItemTextHandler(event) {
+        var itemId = getTodoItemId(event.currentTarget),
+            newText = $(event.currentTarget).val();
+
+        updateSummaryMessage('Updating Item in Azure');
+        todoItemTable
+            .update({ id: itemId, text: newText })  // Async send the update to backend
+            .then(displayItems, handleError); // Update the UI
+        event.preventDefault();
+    }
+
+    /**
+     * Event handler for when the user updates the completed checkbox of a todo item
+     * @param {Event} event the event that caused the request
+     * @returns {void}
+     */
+    function updateItemCompleteHandler(event) {
+        var itemId = getTodoItemId(event.currentTarget),
+            isComplete = $(event.currentTarget).prop('checked');
+
+        updateSummaryMessage('Updating Item in Azure');
+        todoItemTable
+            .update({ id: itemId, complete: isComplete })  // Async send the update to backend
+            .then(displayItems, handleError);        // Update the UI
+    }
+})();
